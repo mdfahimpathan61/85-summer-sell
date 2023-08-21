56970f4f44135db7f8cb622dc7bb3db172e09237
mdfahimpathan61 <mdfahimpathan6@gmail.com>
2023-08-21 23:50:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+let total = 0;
+
+function getPrice(data) {
+    const itemsName = data.parentNode.childNodes[5].innerText;
+    const setItemName = document.getElementById('order-list');
+    const li = document.createElement('li');
+    li.innerText = itemsName;
+    setItemName.appendChild(li);
+
+    const price = data.parentNode.childNodes[7].innerText.split(' ')[0];
+    total = total + parseFloat(price);
+    // console.log(total);
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = total.toFixed(2);
+    const couponBtn = document.getElementById('coupon-btn')
+    const purchase = document.getElementById('purchase')
+
+   
+
+
+
+
+
+
+    if (total > 0) {
+        purchase.removeAttribute('disabled')
+
+        if (total >= 200) {
+            couponBtn.removeAttribute('disabled')
+            const couponCode = document.getElementById('coupon-code-field').value;
+
+            if (couponCode === 'SELL200') {
+                cpnBtn()
+                
+            }
+        }
+        else {
+            couponBtn.setAttribute('disabled');
+            
+        }
+    }
+    else {
+        purchase.setAttribute('disabled')
+    }
+}
+
+
+document.getElementById('coupon-btn').addEventListener('click', function cpnBtn() {
+    const discountId = document.getElementById('discount')
+    const discountTotalId = document.getElementById('total');
+    // const discountTotal = discountTotalId.innerText;
+    const percentise = 20 / 100
+    const discount = total * percentise
+
+
+    discountId.innerText = discount;
+    discountTotalId.innerText = total - discount;
+
+})
+
+
+
